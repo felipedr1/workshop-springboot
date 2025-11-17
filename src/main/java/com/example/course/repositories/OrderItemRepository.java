32d8c71980579c87bbf1aa3f0e9560515ac6a471
Felipe Duarte Rodrigues <felipedr3m@gmail.com>
2025-11-17 00:47:14 -0300

    OrderItem, many-to-many associations

---
+++ src/main/java/com/example/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.course.entities.OrderItem;
+import com.example.course.entities.PK.OrderItemPK;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+
+}
